3b37750d5026e4319a6b11f4d2e3fd4f3e5bd403
Veneta <venkireva80@gmail.com>
2023-05-31 15:19:01 +0300

    Connect to AWS IoT MQTT

--- camera/server.py
+++ camera/server.py
@@ -1,17 +1,50 @@
+import base64
+import json
+import time
+
 import cv2
 import cvlib as cv
 import urllib.request
 import numpy as np
 
-url = 'http://192.168.83.153/cam-mid.jpg'
-im = None
+import AWSIoTPythonSDK.MQTTLib as AWSIoTPyMQTT
+
+from aws import \
+    TOPIC, \
+    CLIENT_ID, \
+    ENDPOINT, \
+    PATH_TO_AMAZON_ROOT_CA_1, \
+    PATH_TO_PRIVATE_KEY, \
+    PATH_TO_CERTIFICATE, \
+    CAMERA_URL
+
+
+def accept_photo():
+    img_resp = urllib.request.urlopen(CAMERA_URL)
+    img_read = img_resp.read()
+    imgnp = np.array(bytearray(img_read), dtype=np.uint8)
+    im = cv2.imdecode(imgnp, -1)
+
+    _, labels, _ = cv.detect_common_objects(im)
+    if 'person' in labels:
+        return base64.b64encode(img_read).decode('utf-8')
+    return None
+
+
+def send_to_AWS(image):
+    myAWSIoTMQTTClient.connect()
+    message = {'encoded_img': image}
+    myAWSIoTMQTTClient.publish(TOPIC, json.dumps(message), 1)
+    print('Published')
+    time.sleep(0.1)
+    myAWSIoTMQTTClient.disconnect()
 
 
 if __name__ == '__main__':
+    myAWSIoTMQTTClient = AWSIoTPyMQTT.AWSIoTMQTTClient(CLIENT_ID)
+    myAWSIoTMQTTClient.configureEndpoint(ENDPOINT, 8883)
+    myAWSIoTMQTTClient.configureCredentials(PATH_TO_AMAZON_ROOT_CA_1, PATH_TO_PRIVATE_KEY, PATH_TO_CERTIFICATE)
     while True:
-        img_resp = urllib.request.urlopen(url)
-        imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
-        im = cv2.imdecode(imgnp, -1)
-
-        bbox, label, conf = cv.detect_common_objects(im)
-        print(label, conf)
+        image = accept_photo()
+        if image is not None:
+            send_to_AWS(image)
